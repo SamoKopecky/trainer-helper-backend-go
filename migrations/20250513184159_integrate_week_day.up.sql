2e7de5b90627c6c671d60bfcd7ed3493c4573a3d
SamoKopecky <sano.kopecky@gmail.com>
2025-05-25 17:30:37 +0200

    typo
    Signed-off-by: SamoKopecky <sano.kopecky@gmail.com>

--- migrations/20250513184159_integrate_week_day.up.sql
+++ migrations/20250513184159_integrate_week_day.up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE week_day
 ADD COLUMN timeslot_id integer;
 
-CREATE INDEX IF NOT EXISTS idx_timesloit_id ON week_day USING btree (timeslot_id);
+CREATE INDEX IF NOT EXISTS idx_timeslot_id ON week_day USING btree (timeslot_id);
 
 ALTER TABLE exercise
 RENAME COLUMN timeslot_id TO week_day_id;
