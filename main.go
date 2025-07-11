ef2b9493396ff84d46e59693ba17eb8197627640
Samuel Kopecky <samuel.kopecky@heureka.group>
2025-07-11 17:23:56 +0200

    fix: Missing .env
    Signed-off-by: Samuel Kopecky <samuel.kopecky@heureka.group>

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ const migrationPath = "file://migrations"
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v. Continuing without .env", err)
+		log.Print("Error loading .env file: %v. Continuing without .env", err)
 	}
 	cfg := config.GetConfig()
 	debug := flag.Bool("debug", false, "Show database queries")
